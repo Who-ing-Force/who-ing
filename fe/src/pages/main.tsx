b668e0a416257c821b6a4b38888c6e9d2d123ff6
Semibro <wnsgud6232@naver.com>
2025-07-07 21:35:19 +0900

    ADD: CustomButton 생성 및 버튼 적용

--- fe/src/pages/main.tsx
+++ fe/src/pages/main.tsx
@@ -1,10 +1,20 @@
+import React from 'react';
 import './main.css';
+import CustomButton from '../components/common/CustomButton';
 
 export default function Main() {
     return (
         <div className="main-container">
             <div className="main-content">
-                main
+                <h2>안녕하세요🎉</h2>
+                <div style={{ display: 'flex', flexDirection: 'column', gap: '16px', alignItems: 'center' }}>
+                    <CustomButton onClick={() => alert('버튼이 클릭되었습니다!')}>
+                        방 만들기
+                    </CustomButton>
+                </div>
+                <div style={{ display: 'flex', flexDirection: 'column', gap: '16px', alignItems: 'center' }}>
+                    <CustomButton>방 참가하기</CustomButton>
+                </div>
             </div>
         </div>
     );
---
+++ fe/src/utils/color.tsx
@@ -0,0 +1,4 @@
+// 프로젝트 컬러 팔레트
+export const colors = {
+    main: "#0064FF",
+}
