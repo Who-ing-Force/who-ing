e3576d909e7ba3542413a59f0951a84ccffd0e8d
Semibro <wnsgud6232@naver.com>
2025-07-07 21:38:38 +0900

    FEAT: 버튼명 수정

--- fe/src/pages/main.tsx
+++ fe/src/pages/main.tsx
@@ -8,12 +8,14 @@ export default function Main() {
             <div className="main-content">
                 <h2>안녕하세요🎉</h2>
                 <div style={{ display: 'flex', flexDirection: 'column', gap: '16px', alignItems: 'center' }}>
-                    <CustomButton onClick={() => alert('버튼이 클릭되었습니다!')}>
-                        방 만들기
+                    <CustomButton onClick={() => alert('생성하기 버튼을 클릭했습니다!')}>
+                        방 생성하기
                     </CustomButton>
                 </div>
                 <div style={{ display: 'flex', flexDirection: 'column', gap: '16px', alignItems: 'center' }}>
-                    <CustomButton>방 참가하기</CustomButton>
+                    <CustomButton onClick={() => alert('참가히기 버튼을 클릭했습니다!')}>
+                        방 참가하기
+                    </CustomButton>
                 </div>
             </div>
         </div>
